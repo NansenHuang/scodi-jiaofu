31d225c6355af38df593bce713e23074be907f44
Nie <i@nieshangsen.com>
2018-01-09 10:16:58 +0800

    test select/deselect function.

--- src/components/data/design/content/index.vue
+++ src/components/data/design/content/index.vue
@@ -22,13 +22,13 @@
 </style>
 
 <template>
-  <div class="content-root">
+  <div class="content-root" @click="deselectAll">
     <h3>文件</h3>
     <div class="content">
-      <folder-icon @enter="onEnterFolder" @select="onSelectFolder" @append-select="onAppendSelectFolder" v-for="(item, index) in folders" :key="index" :folderId="item.name" :childCount="item.count" :folderName="item.name" :folderDate="item.date"></folder-icon>
+      <folder-icon @enter="deselectAll" @select="onSelectFolder" @append-select="onAppendSelectFolder" v-for="(item, index) in folders" :key="index" :selected="item.selected" :folderId="item.name" :childCount="item.count" :folderName="item.name" :folderDate="item.date"></folder-icon>
     </div>
     <div class="content">
-      <file-icon @enter="onEnterFile" @select="onSelectFile" @append-select="onAppendSelectFile" v-for="(item, index) in files" :key="index" :fileId="item.name" :fileName="item.name" :fileDate="item.date"></file-icon>
+      <file-icon @enter="deselectAll" @select="onSelectFile" @append-select="onAppendSelectFile" v-for="(item, index) in files" :key="index" :selected="item.selected" :fileId="item.name" :fileName="item.name" :fileDate="item.date"></file-icon>
     </div>
     <div class="no-content" v-if="!folders.length && !files.length">
         <span>当前文件夹无内容</span>
@@ -46,10 +46,10 @@ export default {
         FolderIcon,
         FileIcon,
     },
-    computed: {
-        folders: function () {
-            // return [];
-            return [
+    computed: {},
+    data: function () {
+        return {
+            folders: [
                 {
                     name: '文件夹一',
                     date: '2018/01/01',
@@ -63,11 +63,8 @@ export default {
                     date: '2018/01/09',
                     count: 0,
                 }
-            ];
-        },
-        files: function () {
-            // return [];
-            return [
+            ],
+            files: [
                 {
                     name: 'a.txt',
                     date: '2018/01/01',
@@ -75,32 +72,46 @@ export default {
                     name: 'b.txt',
                     date: '2018/01/02',
                 }
-            ];
-        },
-    },
-    data: function () {
-        return {};
+            ],
+        };
     },
     methods: {
-        onEnterFolder (val) {
-            console.log('Enter folder: ', val);
+        deselectAll () {
+            let newFolders = this.folders.map(item => ({...item, selected: false}));
+            let newFiles = this.files.map(item => ({...item, selected: false}));
+
+            this.folders = newFolders;
+            this.files = newFiles;
+            console.log('deselect all！');
         },
         onSelectFolder (val) {
+            let newFolders = this.folders.map(item => ({...item, selected: item.name === val}));
+            let newFiles = this.files.map(item => ({...item, selected: false}));
+
+            this.folders = newFolders;
+            this.files = newFiles;
             console.log('Select folder: ', val);
         },
         onAppendSelectFolder (val) {
+            let newFolders = this.folders.map(item => ({...item, selected: item.selected || item.name === val}));
+
+            this.folders = newFolders;
             console.log('Appendselect folder: ', val);
         },
-        onEnterFile (val) {
-            console.log('Enter file: ', val);
-        },
         onSelectFile (val) {
+            let newFolders = this.folders.map(item => ({...item, selected: false}));
+            let newFiles = this.files.map(item => ({...item, selected: item.name === val}));
+
+            this.folders = newFolders;
+            this.files = newFiles;
             console.log('Select file: ', val);
         },
         onAppendSelectFile (val) {
+            let newFiles = this.files.map(item => ({...item, selected: item.selected || item.name === val}));
+
+            this.files = newFiles;
             console.log('Appendselect file: ', val);
         },
-        
     },
 };
 </script>
